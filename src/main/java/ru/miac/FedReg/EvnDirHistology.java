3801b7578f47e9665636ac0033d4051f1d7be8c2
Беляев Алексей Валерьевич <beliaevalva@mosreg.ru>
2021-08-26 11:47:21 +0300

    main class refactor

--- src/main/java/ru/miac/FedReg/EvnDirHistology.java
+++ src/main/java/ru/miac/FedReg/EvnDirHistology.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TshootApplication {
+public class EvnDirHistology {
 
     public static void main(String[] args) {
-        SpringApplication.run(TshootApplication.class, args);
+        SpringApplication.run(EvnDirHistology.class, args);
     }
 
 }
